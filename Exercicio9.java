94f4b4afac8cd9184a65da5b047b1422329c0558
Gabriel Antunes <gabrielzoldic@gmail.com>
2025-02-14 01:27:09 -0300

    Revisao

--- Exercicio9.java
+++ Exercicio9.java
@@ -8,7 +8,7 @@ public class Exercicio9 {
         double[][] pessoas = new double[7][2];
         int acima90 = 0, idade = 0;
 
-        for(int i=0; i<7; i++){
+        for (int i = 0; i < 7; i++) {
             System.out.println("\nInsira a idade da pessoa: ");
             pessoas[i][0] = scn.nextDouble();
 
@@ -16,15 +16,15 @@ public class Exercicio9 {
             pessoas[i][1] = scn.nextDouble();
         }
 
-        for(int i=0; i<7; i++){
-            if(pessoas[i][1] > 90){
+        for (int i = 0; i < 7; i++) {
+            if (pessoas[i][1] > 90) {
                 acima90++;
             }
             idade += pessoas[i][0];
         }
         idade = idade / 7;
 
-        System.out.println("O total de pessoas acima de 90kgs e: "+acima90);
-        System.out.println("A media de idade das pessoas e: "+idade);
+        System.out.println("O total de pessoas acima de 90kgs e: " + acima90);
+        System.out.println("A media de idade das pessoas e: " + idade);
     }
 }
