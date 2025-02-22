c389302677a69df45cdb0e72161cbcf08cc9596a
Gabriel Antunes <gabrielzoldic@gmail.com>
2025-02-21 22:14:36 -0300

    aula02

--- Aula02/ExemploF.java
+++ Aula02/ExemploF.java
@@ -1,9 +1,14 @@
 package Aula02;
 
+<<<<<<< Updated upstream
 public class Aluno {
 private    String RA;
 private    String nome;
 private    String curso;
+=======
+public class ExemploF {
+private    String RA;
+>>>>>>> Stashed changes
 
 public String getRA(){
     return RA;
--- Aula02/ExemploMain.java
+++ Aula02/ExemploMain.java
@@ -1,9 +1,16 @@
 package Aula02;
 
+<<<<<<< Updated upstream
 public class Main {
     public static void main(String[] args){
         Aluno alu;
         alu = new Aluno();
+=======
+public class ExemploMain {
+    public static void main(String[] args){
+        ExemploF alu;
+        alu = new ExemploF();
+>>>>>>> Stashed changes
         alu.setRA("123");
 
         System.out.println("\n"+alu.getRA());
